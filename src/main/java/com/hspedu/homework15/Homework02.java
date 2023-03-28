4412416cb1b0561fa0faa9ae07c095492e65eb12
Ancwwx <ancwwx@gmail.com>
2023-03-28 20:54:10 +0900

    Properties TreeSet TreeMap Collections

---
+++ src/main/java/com/hspedu/homework15/Homework02.java
@@ -0,0 +1,69 @@
+package com.hspedu.homework15;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Homework02 {
+	public static void main(String[] args) {
+		ArrayList arrayList = new ArrayList();
+		Car car = new Car("宝马", 400000);
+		Car car2 = new Car("宾利", 5000000);
+		arrayList.add(car);
+		arrayList.add(car2);
+		System.out.println(arrayList);
+		arrayList.remove(car);
+		System.out.println(arrayList);
+		System.out.println(arrayList.contains(car));
+		System.out.println(arrayList.size());
+		System.out.println(arrayList.isEmpty());
+		// arrayList.clear();
+		arrayList.addAll(arrayList);
+		System.out.println(arrayList);
+		arrayList.containsAll(arrayList);
+		// arrayList.remove(arrayList);
+
+		for (Object o : arrayList) {
+			System.out.println(o);
+		}
+		Iterator iterator = arrayList.iterator();
+		while (iterator.hasNext()) {
+			Object next = iterator.next();
+			System.out.println(next);
+		}
+
+	}
+
+}
+
+class Car {
+	private String name;
+	private double price;
+
+	public Car(String name, double price) {
+		super();
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [name=" + name + ", price=" + price + "]";
+	}
+
+}
---
+++ src/main/java/com/hspedu/map_/HashMapSource2.java
@@ -0,0 +1,30 @@
+package com.hspedu.map_;
+
+import java.util.HashMap;
+
+public class HashMapSource2 {
+	public static void main(String[] args) {
+		HashMap hashMap = new HashMap();
+		for (int i = 1; i <= 12; i++) {
+			hashMap.put(new A(i), "hello");
+		}
+		System.out.println("hashMap=" + hashMap);
+
+	}
+
+}
+
+class A {
+	private int num;
+
+	public A(int num) {
+		this.num = num;
+
+	}
+
+	@Override
+	public int hashCode() {
+		return 100;
+	}
+
+}
